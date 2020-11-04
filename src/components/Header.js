b90fc7a5357fd2051a4099363d5ca12a3196d5a0
Kajanth Kumar <kajanthkumar@gmail.com>
2020-11-03 20:38:18 -0500

    added styles to players

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,30 @@
 import styled from "styled-components";
-import logo from '../assets/logo/logo@1x.png';
+import logo from "../assets/logo/logo@1x.png";
 import wrapper from "./styles/wrapper";
 
 const StyledHeader = styled.header`
-    ${wrapper}
+  ${wrapper}
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & div {
     display: flex;
     justify-content: center;
     align-items: center;
-
-        & div{
+    width: 250px;
     height: 142px;
-
-        }
+  }
 `;
 
 const Header = () => {
-    return (
-        <StyledHeader>
-            <div>
-                <img src= {logo} alt='PlayLine Sports Lotto Logo'/>
-            </div>
-        </StyledHeader>
-    )
-}
+  return (
+    <StyledHeader>
+      <div>
+        <img src={logo} alt="PlayLine Sports Lotto Logo" />
+      </div>
+    </StyledHeader>
+  );
+};
 
 export default Header;
